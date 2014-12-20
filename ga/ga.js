0f73f0609afdaf927c1559d07ca0c906a6ad82f4
Kyle Estes <kyle.estes@polaris.com>
2014-12-20 17:49:57 -0600

    Implemented generating a random population.

--- ga/ga.js
+++ ga/ga.js
@@ -1,1 +1,38 @@
-define(function (require) { });
+define(function (require) {
+    var plus = 10;
+    var minus = 11;
+    var multiply = 12;
+    var divide = 13;
+    
+    // Returns a random integer between min (included) and max (excluded)
+    // Using Math.round() will give you a non-uniform distribution!
+    var getRandomInt = function(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    var population = [];
+    var initialPopulationSize = 100;
+    var bitsPerGene = 4;
+    var genesPerIndividual = 75;
+    
+    var populate = function(populationSize) {
+        population = [];
+        while (population.length < initialPopulationSize) {
+            var individual = '';
+            while (individual.length < genesPerIndividual * bitsPerGene) {
+                individual += getRandomInt(0, 2).toString();
+            }
+            
+            population.push(individual);
+        }
+    };
+
+    var getPopulation = function() {
+        return population;    
+    };
+    
+    var that = {};
+    that.populate = populate;
+    that.getPopulation = getPopulation;
+    return that;
+});
